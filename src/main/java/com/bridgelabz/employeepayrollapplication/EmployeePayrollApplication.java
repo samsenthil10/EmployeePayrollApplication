87a24e19a7cbd99d29c69f29399905164c55b5c4
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-04 03:51:46 +0530

    Added: To log interactions with database

--- src/main/java/com/bridgelabz/employeepayrollapplication/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapplication/EmployeePayrollApplication.java
@@ -13,5 +13,6 @@ public class EmployeePayrollApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
 		log.info("Employee payroll app started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee payroll DB User is {}", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 }
