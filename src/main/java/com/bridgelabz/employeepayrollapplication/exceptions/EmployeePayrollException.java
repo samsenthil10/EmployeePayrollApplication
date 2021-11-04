f859151f69b1f2ab1d37da45bb1b5e59b39e182e
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-04 06:20:44 +0530

    Added: To store message passed by custom exceptions

---
+++ src/main/java/com/bridgelabz/employeepayrollapplication/exceptions/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrollapplication.exceptions;
+
+public class EmployeePayrollException extends RuntimeException {
+
+    public EmployeePayrollException(String message) {
+        super(message);
+    }
+}
