288a86efd619f9c36bef2e858456b1c841aa4ba4
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-08 02:43:11 +0530

    Added: To create entities from properties in the model

--- src/main/java/com/bridgelabz/employeepayrollapplication/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapplication/model/EmployeePayrollData.java
@@ -3,12 +3,26 @@ package com.bridgelabz.employeepayrollapplication.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
 import com.bridgelabz.employeepayrollapplication.dto.EmployeePayrollDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "employee_payroll")
 public @Data class EmployeePayrollData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int employeeId;
     private String name;
     private long salary;
@@ -16,10 +30,14 @@ public @Data class EmployeePayrollData {
     public LocalDate startDate;
     public String note;
     public String profilePic;
+
+    @ElementCollection
+    @CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "department")
     public List<String> departments;
 
-    public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
-        this.employeeId = employeeId;
+    public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
+
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
         this.gender = employeePayrollDTO.gender;
